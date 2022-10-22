d93546cfb29d61d49d49684b7eee2c85ab1d07af
Pedro Amorim <pepas52@gmail.com>
2022-10-22 18:04:37 +0100

    ex1 ficha2

---
+++ IP/Exercicios-Aula/Ficha_n2/ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+float raio;
+float per;
+float area;
+const float pi=3.14159
+
+scanf("Introduza um valor: %f", raio);
+
+per = 2*pi*raio;
+area = pi*r^2;
+
+printf("Perimetro: %.2f\n",per);
+printf("Area: %.2f\n",area);
+}
