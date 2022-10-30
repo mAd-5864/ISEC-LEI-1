3852969bf61c45176d310faf53f25ebf0734f9aa
Pedro Amorim <pepas52@gmail.com>
2022-10-30 14:30:05 +0000

    ex1

--- IP/Exercicios-Aula/Ficha_n2/ex1.c
+++ IP/Exercicios-Aula/Ficha_n2/ex1.c
@@ -12,6 +12,6 @@ int main()
     per = 2 * pi * raio;
     area = pi * (raio * raio);
 
-    printf("\nPerimetro: %.2f\n", per);
-    printf("Area: %.2f\n", area);
+    printf("\nPerimetro circunfrencia: %.2f\n", per);
+    printf("Area circunfrencia: %.2f\n", area);
 }
