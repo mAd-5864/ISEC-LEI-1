70f20418747fd08b849ee6badbcb39faa2250767
Pedro Amorim <pepas52@gmail.com>
2022-11-10 17:09:32 +0000

    ficha 5

---
+++ IP/Exercicios-Aula/Ficha_n5/ex1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int squareRoot(float n)
+{
+    return n * n;
+}
+
+int main()
+{
+    float num = 1, square;
+    do
+    {
+        printf("\nInsira um numero: ");
+        scanf("%f", &num);
+        if (num != 0)
+        {
+            square = squareRoot(num);
+            printf("\n%.2f ao quadrado = %.2f", num, square);
+        }
+    } while (num != 0);
+}
