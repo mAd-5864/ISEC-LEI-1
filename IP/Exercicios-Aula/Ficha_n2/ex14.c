ced68e674603df03fc90b35d30341b1a0357aa66
Pedro Amorim <pepas52@gmail.com>
2022-11-17 12:47:20 +0000

    ex14 ficha2

--- IP/Exercicios-Aula/Ficha_n2/ex14.c
+++ IP/Exercicios-Aula/Ficha_n2/ex14.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-
-struct data {
-    int* dia;
-    int* mes;
-    int* ano;
-};
+#include <string.h>
 
 int main()
 {
-    struct data date;
+    char date[8];
     
 
     printf("Introduza uma data:");
-    scanf("%.2d/%.2d/%.2d\n", &date.dia, &date.mes, &date.ano);
+    scanf("%s", date);
 
-    printf("Dia: %.2d\n", date.dia);
-    printf("Mes: %.2d\n", date.mes);
-    printf("Ano: %.2d\n", date.ano);
-    printf("Data: %.2d/%.2d/%.2d\n", date.dia, date.mes, date.ano);
+    char * dia = strtok(date, "/");
+    printf( "%s\n", dia ); 
+
+/*    while( barra != NULL ) {
+      barra = strtok(NULL, "/");
+   } */
+
+
+    printf("Data: %s\n", date);
+/*     printf("Mes: %.2d\n", mes);
+    printf("Ano: %.2d\n", ano);
+    printf("Data: %.2d/%.2d/%.2d\n", dia, mes, ano); */
 }
