2c592fd34ec47f738ea612bea71bd0f000412cdd
Pedro Amorim <pepas52@gmail.com>
2022-12-16 19:59:44 +0000

    ficha 10

---
+++ TWEB/Exercícios Aula/Ficha 10/index.js
@@ -0,0 +1,6 @@
+﻿/**************************************************
+ * Javascript - Ficha 10
+ * ************************************************/
+document.getElementById("btn-alert2").addEventListener("click", function(){ alert("ALERTA 2"); });
+
+document.getElementById
