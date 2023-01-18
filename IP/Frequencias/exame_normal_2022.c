d262f35cbf1534d245507a344d0f6c47815f0ed3
Pedro Amorim <pepas52@gmail.com>
2023-01-18 22:27:21 +0000

    exame

---
+++ IP/Frequencias/exame_normal_2022.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define N 50
+
+int alteraFrase(char s[])
+{
+    int occ = 0;
+    for (int i = 0; i<N; i++)
+    {
+        if (s[i] == 'c' && s[i + 1] == 'h')
+        {
+            s[i - occ] = 'x';
+            i++;
+            occ++;
+        }
+        else
+        {
+            s[i - occ] = s[i];
+        }
+    }
+    return occ;
+}
+void main()
+{
+    char s1[N] = "comi um cacho, so para chatear o chato do chico";
+
+    puts(s1);
+    printf("\nforam feitas %d alteracoes\n", alteraFrase(s1));
+    puts(s1);
+}
