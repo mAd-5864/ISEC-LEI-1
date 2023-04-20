3d4e1ca0ea1b76f4686a7377d81bb4f381c38c12
Pedro Amorim <pepas52@gmail.com>
2023-04-20 11:53:12 +0100

    continuacao ficha LS

--- LS/Ficha 3/js/index.js
+++ LS/Ficha 3/js/index.js
@@ -3,20 +3,19 @@ const panelControl = document.querySelector('#panel-control');
 const panelGame = document.querySelector('#game');
 const btLevel = document.querySelector('#btLevel');
 const btPlay = document.querySelector('#btPlay');
-const nPares = 3
-let cards= document.querySelectorAll(".card")
 
 let cardLogos = ["angular" ,"bootstrap" ,"html" ,"javascript" ,"vue" ,"svelte" ,"react" ,"css" ,"backbone" ,"ember"]
+let flippedCards =[]; 
 
 
 // ------------------------
 // Funções Genéricas
 // ------------------------
 function reset() {
-
+     
      message.classList.remove('hide');
      if (btLevel.value === '0')
-          btPlay.disabled = true
+     btPlay.disabled = true
      else {
           btPlay.disabled = false;
           panelGame.style.display = 'grid';
@@ -25,6 +24,12 @@ function reset() {
      elementos.forEach(elemento => {
           elemento.classList.remove('gameStarted');
      });
+     /*      for (const card of cards) {
+          card.classList.remove("inative"); 
+          card.querySelector(".card-front").classList.remove("grayscale")
+          card.onclick = "";
+     } */
+     panelGame.innerHTML=""
 }
 
 function startGame() {
@@ -35,6 +40,21 @@ function startGame() {
      elementos.forEach(elemento => {
           elemento.classList.add('gameStarted')
      });
+     
+     let nPares = btLevel.value
+     for (let i = 0; i < nPares*2; i++) {
+          panelGame.innerHTML+=`
+          <div class="card" data-logo="">
+          <img src="images/ls.png" class="card-back" />
+          <img src="" class="card-front" />
+          </div>`
+     }
+     if (btLevel.value==6) {
+          panelGame.className="intermedio"
+     } else if (btLevel.value==10) {
+          panelGame.className="avancado"
+     } else {panelGame.className=""}
+     let cards= document.querySelectorAll(".card")
      shuffleArray(cardLogos);
      let gameCards = cardLogos.slice(0,nPares);
      gameCards = gameCards.concat(gameCards);
@@ -44,7 +64,7 @@ function startGame() {
      for (let i = 0; i < cards.length; i++) {
           cards[i].onclick = flipCard;
           cards[i].dataset.logo = gameCards[i];
-          cards[i].querySelector(".card-front").src=`images/${gameCards[i]}.png`    
+          cards[i].querySelector(".card-front").src=`images/${gameCards[i]}.png`   
      }
      //showCards();
 }
@@ -52,7 +72,7 @@ function stopGame() {
      btPlay.textContent = 'Iniciar Jogo'
      btLevel.disabled = false;
      reset();
-     hideCards();
+     hideCards(cards);
 }
 // Algoritmo Fisher-Yates - Algoritmo que baralha um array.
 function shuffleArray(array) {
@@ -66,15 +86,38 @@ function shuffleArray(array) {
 }
 function flipCard() {
      this.classList.add("flipped");
+     flippedCards.push(this);
+     //this.removeAttribute("onclick");
+     this.onclick = "";
+     checkPair();
+}
+function checkPair() {
+     if (flippedCards.length==2) {
+          if (flippedCards[0].dataset.logo==flippedCards[1].dataset.logo) {
+               for (const card of  flippedCards) {
+                    card.classList.add("inative");
+                    card.querySelector(".card-front").classList.add("grayscale")
+               }
+               console.log("Iguais")
+               flippedCards=[];
+          } else {
+               console.log("Diferentes")
+               setTimeout(() => {
+                    hideCards(flippedCards);
+                    flippedCards=[];
+               }, 1500); 
+          }
+     }
 }
 function showCards() {
      for (const card of cards) {
           card.classList.add("flipped")
      }
 }
-function hideCards() {
-     for (const card of cards) {
+function hideCards(array) {
+     for (const card of array) {
           card.classList.remove("flipped")
+          card.onclick = flipCard
      }
 }
 // ------------------------
