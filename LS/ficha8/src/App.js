d86edde36dd60f561662a4d22e0d607e8c722bcf
Pedro Amorim <pepas52@gmail.com>
2023-05-05 12:23:32 +0100

    continuacao react

---
+++ LS/ficha8/src/App.js
@@ -0,0 +1,15 @@
+import "./assets/styles/App.css";
+
+function App() {
+  return (
+    <div id="container">
+      <h2>Jogo de Memória em React</h2>
+      <h3>Linguagens Script</h3>
+    </div>
+  );
+}
+
+export default App;
+// Esta linha também poderia ser eliminada
+// e adefinição da funsão ser substituida 
+// export default function App() {
